16b691ce65345f8aaccedddc1362be049a22588d
Gaitan Matias <gaitanmatiasg@gmail.com>
2025-11-09 06:47:05 -0300

    refactor(Controllers): elimina bloques try/catch y delega manejo de errores al middleware
    - Elimina bloques try/catch en controladores de Auth y Appointments.
    - Permite que los errores sean propagados al middleware de manejo centralizado.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,7 +2,6 @@
 // Controlador encargado de manejar la autenticación de usuarios.
 // Contiene endpoints para registro, login, verificación de correo y recuperación de contraseña.
 
-import { validationResult } from "express-validator";
 import { validateRequest } from "../utils/validateRequest.utils.js";
 import AuthService from "../services/auth.service.js";
 
@@ -10,146 +9,103 @@ class AuthController {
   /* =============== REGISTRO E INICIO DE SESIÓN =============== */
   /* ---------- REGISTER ---------- */
   static async register(req, res) {
-    try {
-      // Validaciones de express-validator
-      if (!validateRequest(req, res)) return;
+    // Validaciones de express-validator
+    if (!validateRequest(req, res)) return;
 
-      const { firstName, lastName, phoneNumber, email, password } = req.body;
-      await AuthService.register(
-        firstName,
-        lastName,
-        phoneNumber,
-        email,
-        password
-      );
-      AuthService.sendEmailVerification(email).catch((err) =>
-        console.error("Error enviando mail de verificación:", err)
-      );
+    const { firstName, lastName, phoneNumber, email, password } = req.body;
+    await AuthService.register(
+      firstName,
+      lastName,
+      phoneNumber,
+      email,
+      password
+    );
+    AuthService.sendEmailVerification(email).catch((err) =>
+      console.error("Error enviando mail de verificación:", err)
+    );
 
-      return res.status(201).json({
-        ok: true,
-        status: 201,
-        message: "El usuario se ha registrado correctamente",
-      });
-
-    } catch (error) {
-      const status = error.status || 500;
-      const message = error.message || "Error interno del servidor";
-      return res.status(status).json({ ok: false, status, message });
-    }
+    return res.status(201).json({
+      ok: true,
+      status: 201,
+      message: "El usuario se ha registrado correctamente",
+    });
   }
 
   /* ---------- LOGIN ---------- */
   static async login(req, res) {
-    try {
-      // Validaciones de express-validator
-      if (!validateRequest(req, res)) return;
+    // Validaciones de express-validator
+    if (!validateRequest(req, res)) return;
 
-      const { email, password } = req.body;
-      const authorization_token = await AuthService.login(email, password);
+    const { email, password } = req.body;
+    const authorization_token = await AuthService.login(email, password);
 
-      return res.status(200).json({
-        ok: true,
-        status: 200,
-        message: "Sesión iniciada correctamente",
-        data: { authorization_token },
-      });
-
-    } catch (error) {
-      const status = error.status || 500;
-      const message = error.message || "Error interno del servidor";
-
-      return res.status(status).json({ ok: false, status, message });
-    }
+    return res.status(200).json({
+      ok: true,
+      status: 200,
+      message: "Sesión iniciada correctamente",
+      data: { authorization_token },
+    });
   }
 
   /* =============== VERIFICACIÓN DE CORREO =============== */
   /* ---------- SEND EMAIL VERIFICATION ---------- */
   static async sendEmailVerification(req, res) {
-    try {
-      // Validaciones de express-validator
-      if (!validateRequest(req, res)) return;
+    // Validaciones de express-validator
+    if (!validateRequest(req, res)) return;
 
-      const { email } = req.body;
-      await AuthService.sendEmailVerification(email);
+    const { email } = req.body;
+    await AuthService.sendEmailVerification(email);
 
-      return res.status(200).json({
-        ok: true,
-        status: 200,
-        message: "Se ha enviado un correo de verificación",
-      });
-
-    } catch (error) {
-      const status = error.status || 500;
-      const message = error.message || "Error interno del servidor";
-      return res.status(status).json({ ok: false, status, message });
-    }
+    return res.status(200).json({
+      ok: true,
+      status: 200,
+      message: "Se ha enviado un correo de verificación",
+    });
   }
 
   /* ---------- VERIFY EMAIL ---------- */
   static async verifyEmail(req, res) {
-    try {
-      const { verification_token } = req.params;
-      await AuthService.verifyEmail(verification_token);
+    const { verification_token } = req.params;
+    await AuthService.verifyEmail(verification_token);
 
-      return res.status(200).json({
-        ok: true,
-        status: 200,
-        message: "Verificación de correo exitosa",
-      });
-
-    } catch (error) {
-      const status = error.status || 500;
-      const message = error.message || "Error interno del servidor";
-      return res.status(status).json({ ok: false, status, message });
-    }
+    return res.status(200).json({
+      ok: true,
+      status: 200,
+      message: "Verificación de correo exitosa",
+    });
   }
 
   /* =============== REESTABLECIMIENTO DE CONTRASEÑA =============== */
   /* ---------- FORGOT PASSWORD ---------- */
   static async forgotPassword(req, res) {
-    try {
-      // Validaciones de express-validator
-      if (!validateRequest(req, res)) return;
+    // Validaciones de express-validator
+    if (!validateRequest(req, res)) return;
 
-      const { email } = req.body;
-      const response = await AuthService.forgotPassword(email);
+    const { email } = req.body;
+    const response = await AuthService.forgotPassword(email);
 
-      return res.status(200).json({
-        ok: true,
-        status: 200,
-        message: "Se ha enviado un enlace para restablecer la contraseña",
-        data: response,
-      });
-
-    } catch (error) {
-      const status = error.status || 500;
-      const message = error.message || "Error interno del servidor";
-      return res.status(status).json({ ok: false, status, message });
-    }
+    return res.status(200).json({
+      ok: true,
+      status: 200,
+      message: "Se ha enviado un enlace para restablecer la contraseña",
+      data: response,
+    });
   }
 
   /* ---------- RESET PASSWORD ---------- */
   static async resetPassword(req, res) {
-    try {
-      // Validaciones de express-validator
-      if (!validateRequest(req, res)) return;
+    // Validaciones de express-validator
+    if (!validateRequest(req, res)) return;
 
-      const { newPassword } = req.body;
-      const { reset_token } = req.params;
-      await AuthService.resetPassword(newPassword, reset_token);
+    const { newPassword } = req.body;
+    const { reset_token } = req.params;
+    await AuthService.resetPassword(newPassword, reset_token);
 
-      return res.status(200).json({
-        ok: true,
-        status: 200,
-        message: "Contraseña restablecida correctamente",
-      });
-
-    } catch (error) {
-      const status = error.status || 500;
-      const message = error.message || "Error interno del servidor";
-      return res.status(status).json({ ok: false, status, message });
-    }
+    return res.status(200).json({
+      ok: true,
+      status: 200,
+      message: "Contraseña restablecida correctamente",
+    });
   }
 }
 
